51ead280138260dd2967a9efe5123f30eb594a43
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2020-05-08 01:51:06 +0200

    Get process data and its parents recursively

--- lsof/process.go
+++ lsof/process.go
@@ -3,23 +3,42 @@ package lsof
 import (
 	"io/ioutil"
 	"fmt"
+	"strings"
+	"strconv"
 )
 
 type Process struct {
 	Pid      int
 	Name     string
-	Children []Process
+	Parent   *Process
 }
 
 func newProcess(pid int) (*Process, error) {
-	cmdLineBytes, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/cmdline", pid))
+	statsBytes, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/status", pid))
+	//cmdLineBytes, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/cmdline", pid))
+	if err != nil {
+		return nil, err
+	}
+	statsLines := strings.Split(string(statsBytes), "\n")
+	parentPid, err := strconv.Atoi(strings.TrimPrefix(string(statsLines[6]), "PPid:\t"))
+	processName := strings.TrimPrefix(string(statsLines[0]), "Name:\t")
+	if err != nil {
+		return nil, err
+	}
+	var parentProcess *Process
+	// this guy has a parent, but we cannot access its data :)
+	if pid == 1 {
+		parentProcess = nil
+	} else {
+		parentProcess, err = newProcess(parentPid)
+	}
 	if err != nil {
 		return nil, err
 	}
 	return &Process{
 		Pid: pid,
-		Name: string(cmdLineBytes),
-		//TODO complete the rest
+		Name: processName,
+		Parent: parentProcess,
 	}, nil
 
 }
