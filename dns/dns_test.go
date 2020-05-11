5ba72d867856718e2fefbd4c16bd74e9b5e9dca8
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2020-05-11 15:55:40 +0200

    dns_test: Fix test

--- dns/dns_test.go
+++ dns/dns_test.go
@@ -10,12 +10,12 @@ func TestgetKnownDomain(t *testing.T) {
 	knownDomain, _ := getKnownDomain([]string{"ec2-52-38-207-226.us-west-2.compute.amazonaws.com"})
 	assert.Equal(t, "Amazon AWS", knownDomain)
 	knownDomain, _ = getKnownDomain([]string{"arn09s10-in-f4.1e100.net"})
-	assert.Equal(t, "Google Cloud", knownDomain)
+	assert.Equal(t, "1e100.net (Google Cloud)", knownDomain)
 }
 
 func TestDomainName(t *testing.T) {
 	ip := net.ParseIP("172.217.22.174")
 	domName, _ := DomainName(&ip)
-	assert.Equal(t, "Google Cloud", domName)
+	assert.Equal(t, "1e100.net (Google Cloud)", domName)
 
 }
