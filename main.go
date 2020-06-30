dd6425305ef01dd042ab3df428dcfacba22d3249
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2020-06-30 18:29:10 +0200

    prevent running it from OS != linux

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/blasrodri/frown/ui"
+	"os"
+	"runtime"
 )
 
 func main() {
-
+	if !isSupportedOs() {
+		fmt.Fprintln(os.Stderr, "Frown is only available for Linux. Sorry!")
+		os.Exit(-1)
+	}
 	var uiConfig = &ui.UIConfig{
 		FilterSecurityLevel: 1,
 	}
 	manageState(uiConfig, ui.TerminalUI)
+}
+
+func isSupportedOs() bool {
+	os := runtime.GOOS
+	switch os {
+	case "linux":
+		return true
+	default:
+		return false
+	}
 }
